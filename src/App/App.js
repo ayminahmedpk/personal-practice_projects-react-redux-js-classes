50949f8623e9e56e70b4967077006b921ca80bcb
ayminahmedpk <ayminahmedpk@gmail.com>
2022-07-06 16:31:51 +0500

    0.1.8

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 
-import Root from '../Lessons/07-middleware/Root';
+import Root from '../Lessons/08-store-subscribe/Root';
 
 class App extends Component {
   render() {
