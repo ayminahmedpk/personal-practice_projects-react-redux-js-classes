719f5239dee1abb6e03f7b4be6398271a4c03d6d
ayminahmedpk <ayminahmedpk@gmail.com>
2022-07-06 19:20:36 +0500

    0.1.9

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 
-import Root from '../Lessons/08-store-subscribe/Root';
+import Root from '../Lessons/09-async-fetch-thunk/Root';
 
 class App extends Component {
   render() {
